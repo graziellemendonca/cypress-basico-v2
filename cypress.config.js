8a3e398c68e58529e2d9aa9269c6bbcf7d0a6745
graziellemendonca <grazyhsouza@gmail.com>
2023-07-06 11:14:11 -0300

    Conf cy

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,19 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  projectId: '1uzifv',
+  projectId: "1uzifv",
+
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+  },
+
+  component: {
+    devServer: {
+      framework: "angular",
+      bundler: "webpack",
+    },
+    specPattern: "**/*.cy.ts",
   },
 });
